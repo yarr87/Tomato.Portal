9fcb85d2793a8663275d3741a72d6a8102070fc5
Jeff <yarr87@gmail.com>
2015-03-02 21:09:54 -0500

    workin

--- app/scripts/components/main.jsx
+++ app/scripts/components/main.jsx
@@ -1,31 +1,56 @@
 var React = require('react');
-var deviceStore = require('repositories/deviceRepository');
+var Reflux = require('reflux');
+var deviceStore = require('stores/deviceStore');
 var Device = require('components/devices/device');
+var TagSearch = require('components/tags/tagSearch');
+var actions = require('actions/actions');
+var _ = require('lodash');
 
 var Main = React.createClass({
+    mixins: [Reflux.connect(deviceStore, 'devices')],
+
     getInitialState: function() {
-        return {devices: []};
+        return {
+            devices: [],
+            selectedTagIds: []
+        };
     },
 
     componentWillMount: function() {
-        deviceStore.getDevices().then(function(devices) {
-            this.setState({devices: devices });
-        }.bind(this));
+        actions.loadDevices();
+    },
+
+    onTagSelect: function(selectedTagIds) {
+        this.setState({selectedTagIds: selectedTagIds});
     },
 
     render: function () {
 
+        var tagIds = this.state.selectedTagIds;
+
         var markup = this.state.devices.map(function(item) {
-            return (
-                <div className="block-grid-item">
-                    <Device item={item} />
-                </div>
-            );
+
+            if (tagIds.length == 0 || _.any(item.tags, function(tag) {
+                return _.indexOf(tagIds, tag.id) >= 0;
+            })) {
+                
+                return (
+                    <div className="block-grid-item">
+                        <Device item={item} />
+                    </div>
+                );
+            }
+            else {
+                return '';
+            }
         });
 
         return (
-            <div className="block-grid-md-3 block-grid-sm-2">
-               {markup}
+            <div>
+                <TagSearch onTagSelect={this.onTagSelect} />
+                <div className="block-grid-md-3 block-grid-sm-2 block-grid-xs-2">
+                   {markup}
+                </div>
             </div>
         );
     }
---
+++ app/scripts/components/tags/tagSearch.jsx
@@ -0,0 +1,65 @@
+var React = require('react');
+var Reflux = require('reflux');
+var tagStore = require('stores/tagStore');
+var actions = require('actions/actions');
+var _ = require('lodash');
+
+var TagSearch = React.createClass({
+    mixins: [Reflux.connect(tagStore)],
+
+    getInitialState: function() {
+        return {
+            tags: [],
+            selectedTagIds: []
+        };
+    },
+
+    componentWillMount: function() {
+        actions.loadTags();
+    },
+
+    handleTagClick: function(tag) {
+
+        var selectedTagIds = this.state.selectedTagIds;
+
+        var index = _.indexOf(selectedTagIds, tag.id);
+
+        if (index >= 0) {
+            selectedTagIds.splice(index, 1);
+        }
+        else {
+            selectedTagIds.push(tag.id);
+        }
+
+        this.setState({ selectedTagIds: selectedTagIds });
+
+        this.props.onTagSelect(this.state.selectedTagIds);
+    },
+
+    render: function () {
+
+        var tags = this.state.tags.map(function(tag) {
+            var cn = "";
+            if (_.indexOf(this.state.selectedTagIds, tag.id) >= 0) {
+                cn += " active";
+            }
+            else {
+                cn += "";
+            }
+
+            return (
+                <li className={cn}>
+                    <a href="#" onClick={this.handleTagClick.bind(this, tag)}>{tag.name}</a>
+                </li>
+            );
+        }.bind(this));
+
+        return (
+            <ul className="nav nav-pills">
+                {tags}
+            </ul>
+        );
+    }
+});
+
+module.exports = TagSearch;
